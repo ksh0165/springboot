ec05120460500a952b397aff51f8c59867af8ddf
wkssm7575 <wkssm7575@naver.com>
2020-09-29 21:01:35 +0900

    alter1

--- src/main/java/com/code/ex/springboot/service/PostsService.java
+++ src/main/java/com/code/ex/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.code.ex.springboot.service;
 
 import com.code.ex.springboot.domain.posts.Posts;
 import com.code.ex.springboot.domain.posts.PostsRepository;
+import com.code.ex.springboot.web.dto.PostsListResponseDto;
 import com.code.ex.springboot.web.dto.PostsResponseDto;
 import com.code.ex.springboot.web.dto.PostsSaveRequestDto;
 import com.code.ex.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -29,5 +33,11 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/code/ex/springboot/web/IndexController.java
+++ src/main/java/com/code/ex/springboot/web/IndexController.java
@@ -1,13 +1,17 @@
 package com.code.ex.springboot.web;
 
+import com.code.ex.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private  final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
     @GetMapping("/posts/save")
