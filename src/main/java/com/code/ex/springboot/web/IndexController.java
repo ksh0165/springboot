1435d76b25f1f9915617f1614ca31cd0097fa62e
wkssm7575 <wkssm7575@naver.com>
2020-09-29 21:13:52 +0900

    alter2

--- src/main/java/com/code/ex/springboot/web/IndexController.java
+++ src/main/java/com/code/ex/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.code.ex.springboot.web;
 
 import com.code.ex.springboot.service.PostsService;
+import com.code.ex.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -17,5 +20,11 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,12 @@ var main = {
         $('#btn-save').on('click',function(){
             _this.save();
         });
+        $('#btn-delete').on('click',function(){
+            _this.delete();
+        });
+        $('#btn-update').on('click',function(){
+            _this.update();
+        });
     },
     save : function () {
         var data = {
@@ -42,7 +48,21 @@ var main = {
         }).fail(function(error){
             alert(JSON.stringify(error))
         });
+    },
+    delete: function(){
+        var id = $('#id').val();
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function(){
+            alert('글이 삭제되었습니다.');
+            window.location.href='/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
     }
 };
 
-index.init();
+main.init();
